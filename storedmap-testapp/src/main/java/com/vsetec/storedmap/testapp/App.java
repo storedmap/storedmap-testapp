390e1b94904e49296169629aa625f87c63005f0f
fedd <fedd@vsetec.com>
2018-11-23 17:21:12 +0300

    List of categories including one with non-Latin name test

--- storedmap-testapp/src/main/java/com/vsetec/storedmap/testapp/App.java
+++ storedmap-testapp/src/main/java/com/vsetec/storedmap/testapp/App.java
@@ -6,8 +6,10 @@
 package com.vsetec.storedmap.testapp;
 
 import com.vsetec.storedmap.Category;
+import com.vsetec.storedmap.MixedDriver;
 import com.vsetec.storedmap.Store;
 import com.vsetec.storedmap.StoredMap;
+import com.vsetec.storedmap.elasticsearch.ElasticsearchDriver;
 import com.vsetec.storedmap.jdbc.GenericJdbcDriver;
 import java.util.HashMap;
 import java.util.Map;
@@ -20,6 +22,13 @@ import java.util.Properties;
 public class App {
 
     public static void main(String[] args) {
+
+        Properties elasticsearch = new Properties();
+        elasticsearch.setProperty("storedmap.applicationCode", "testapp");
+        elasticsearch.setProperty("storedmap.driver", ElasticsearchDriver.class.getName());
+        elasticsearch.setProperty("storedmap.elasticsearch.host", "localhost");
+        elasticsearch.setProperty("storedmap.elasticsearch.port", "9200");
+
         Properties postgres = new Properties();
         postgres.setProperty("storedmap.applicationCode", "testapp");
         postgres.setProperty("storedmap.driver", GenericJdbcDriver.class.getName());
@@ -34,9 +43,22 @@ public class App {
                 + "create index @{indexName}_ind1 on @{indexName}_indx (sort, tag);\n"
                 + "create index @{indexName}_ind2 on @{indexName}_indx (id)");
 
-        Store store = Store.getStore(postgres);
+        Properties derby = new Properties();
+        derby.setProperty("storedmap.applicationCode", "testapp");
+        derby.setProperty("storedmap.driver", GenericJdbcDriver.class.getName());
+        derby.setProperty("storedmap.jdbc.driver", "org.apache.derby.jdbc.EmbeddedDriver");
+        derby.setProperty("storedmap.jdbc.url", "jdbc:derby:testapp;create=true");
 
-        Category category = store.get("themap");
+        Properties mixed = new Properties();
+        mixed.putAll(elasticsearch);
+        mixed.putAll(postgres);
+        mixed.setProperty("storedmap.driver", MixedDriver.class.getName());
+        mixed.setProperty("storedmap.driver.main", GenericJdbcDriver.class.getName());
+        mixed.setProperty("storedmap.driver.additional", ElasticsearchDriver.class.getName());
+
+        Store store = Store.getStore(elasticsearch);
+
+        Category category = store.get("Русский");
 
         for (int i = 0; i < 2; i++) {
             StoredMap map = category.map("map" + i);
@@ -47,7 +69,7 @@ public class App {
         }
 
         for (int i = 2; i < 4; i++) {
-            HashMap<String,Object> map = new HashMap<>();
+            HashMap<String, Object> map = new HashMap<>();
             for (int j = 0; j < 4; j++) {
                 map.put("key" + j, "value" + j + " of map " + i + " that was put in a different way");
             }
@@ -55,14 +77,19 @@ public class App {
             category.put("map" + i, map);
 
         }
-        
-        
-        System.out.println("Maps in category " + category.name() + ":");
+
+        System.out.println("\nMaps in category " + category.name() + ":");
         for (StoredMap map : category.maps()) {
-            System.out.println("Map id:\t" + map.key());
+            System.out.println("\nMap id:\t" + map.key());
             for (Map.Entry<String, Object> entry : map.entrySet()) {
                 System.out.println("Key:\t" + entry.getKey() + "\tvalue:\t" + entry.getValue());
             }
+        }
+
+        System.out.println("\nCategories:");
+        
+        for(Category cat: store.categories()){
+                System.out.println("Name:\t" + cat.name());
         }
 
         store.close();
